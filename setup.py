e484008ec0e3c42f2fe7076ff611e5f4945bfb17
Pieter <13552343+pmoris@users.noreply.github.com>
2019-11-13 14:55:44 +0100

    Cookiecutter data science init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='TCR-epitope recognition prediction using CNN approach',
+    author='Pieter Moris',
+    license='MIT',
+)
