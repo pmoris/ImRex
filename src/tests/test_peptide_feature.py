dace86edd21f7b85f3ee9544d2f0c46554af8be2
Pieter <13552343+pmoris@users.noreply.github.com>
2019-11-20 08:32:51 +0100

    Unit test for matrix operations

---
+++ src/tests/test_peptide_feature.py
@@ -0,0 +1,30 @@
+import numpy as np
+import src.bio.peptide_feature
+
+
+def test_product_operator():
+
+    vector_1 = np.array([1, 2, 3])
+    vector_2 = np.array([4, 5, 6])
+
+    product_operator = src.bio.peptide_feature.ProductOperator()
+
+    outer_product = product_operator.matrix(vector_1, vector_2)
+
+    expected = np.array([[4, 5, 6], [8, 10, 12], [12, 15, 18]])
+
+    np.testing.assert_array_equal(expected, outer_product)
+
+
+def test_abs_diff_operator():
+
+    vector_1 = np.array([1, 2, 3])
+    vector_2 = np.array([4, 5, 6])
+
+    abs_diff_operator = src.bio.peptide_feature.AbsDifferenceOperator()
+
+    abs_diff = abs_diff_operator.matrix(vector_1, vector_2)
+
+    expected = np.array([[-3, -4, -5], [-2, -3, -4], [-1, -2, -3]])
+
+    np.testing.assert_array_equal(expected, abs_diff)
