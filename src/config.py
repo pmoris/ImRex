acb14c319f5b89733c6057d8ca536e2c19653680
Pieter <13552343+pmoris@users.noreply.github.com>
2019-11-25 18:01:09 +0100

    Create config.py file to easily access the project root path in scripts
    By using from src.config import PROJECT_ROOT, a pathlib object is created that can be used as follows:
    vdjdb_normal_path = PROJECT_ROOT / "data/raw/vdjdb/vdjdb-2019-08-08/vdjdb.txt"

---
+++ src/config.py
@@ -0,0 +1,8 @@
+"""Defines various global variables,
+such as the project root file path
+and a dictionary of amino acids.
+"""
+
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
