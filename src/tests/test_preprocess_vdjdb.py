1643960efb7dce0ba577d720ee53d72817849e50
Pieter <13552343+pmoris@users.noreply.github.com>
2019-11-26 11:24:48 +0100

    Update preprocessing test
    Removed temporary files since the preprocessing function now returns a dataframe instead of writing a csv file.
    Changed tests to expected numbers for the github release.
    Added tests for spurious and 10x data.

--- src/tests/test_preprocess_vdjdb.py
+++ src/tests/test_preprocess_vdjdb.py
@@ -1,8 +1,13 @@
-import os
+# Test to see if scripts and datasets result in the expected number of entries
+# Import notes:
+# Duplicates are always removed as the very last step, based only on the cdr3 and epitope columns.
+# When not specified explicitly, spurious entries are removed by default.
+
+# import os
 import logging
 from pathlib import Path
 
-import pandas as pd
+# import pandas as pd
 
 import src.preprocessing.preprocess_vdjdb
 
@@ -17,47 +22,116 @@ logger = logging.getLogger(__name__)
 
 def test_filter_vdjdb():
 
-    # csv_buffer = StringIO()
-    # temp_test_file = tempfile.NamedTemporaryFile
-    temp_test_file = "src/tests/preprocess_vdjdb_test.csv"
+    #####################
+    # vdjdb web release #
+    #####################
 
-    # human TRB
-    src.preprocessing.preprocess_vdjdb.filter_vdjdb(
-        input=Path("data/raw/vdjdb/vdjdb-all-species-tra-trb-non-paired.tsv"),
-        output=Path(temp_test_file),
+    # NO LONGER FUNCTIONAL DUE TO CHANGES IN COLUMN NAMES
+    # deprecated code is kept as log on how to use temporary testing files
+
+    # # csv_buffer = StringIO()
+    # # temp_test_file = tempfile.NamedTemporaryFile
+    # # temp_test_file = "src/tests/preprocess_vdjdb_test.csv"
+
+    # # human TRB - web
+    # df = src.preprocessing.preprocess_vdjdb.filter_vdjdb(
+    #     # input=Path("data/raw/vdjdb/vdjdb-all-species-tra-trb-non-paired.tsv"),
+    #     input=Path("data/raw/vdjdb/vdjdb-browser.tsv"),
+    #     # output=Path(temp_test_file),
+    #     tcr_chain="TRB",
+    #     species="human",
+    #     mhc="all",
+    #     hla="all",
+    # )
+    # # df = pd.read_csv(csv_buffer, sep=";")
+    # # df = pd.read_csv(temp_test_file, sep=";")
+    # assert df.shape == (32639, 2)
+    # # os.remove(temp_test_file)
+
+    # # human TRA - web
+    # df = src.preprocessing.preprocess_vdjdb.filter_vdjdb(
+    #     input=Path("data/raw/vdjdb/vdjdb-browser.tsv"),
+    #     tcr_chain="TRA",
+    #     species="human",
+    #     mhc="all",
+    #     hla="all",
+    # )
+    # assert df.shape == (22248, 2)
+
+    # # all species all chains - web
+    # df = src.preprocessing.preprocess_vdjdb.filter_vdjdb(
+    #     input=Path("data/raw/vdjdb/vdjdb-browser.tsv"),
+    #     tcr_chain="all",
+    #     species="all",
+    #     mhc="all",
+    #     hla="all",
+    # )
+    # assert df.shape == (59072, 2)
+
+    ########################
+    # vdjdb github release #
+    ########################
+
+    # human TRB with spurious - vdjdb
+    df = src.preprocessing.preprocess_vdjdb.filter_vdjdb(
+        input=Path("data/raw/vdjdb/vdjdb-2019-08-08/vdjdb.txt"),
         tcr_chain="TRB",
         species="human",
         mhc="all",
         hla="all",
     )
-
-    # df = pd.read_csv(csv_buffer, sep=";")
-    df = pd.read_csv(temp_test_file, sep=";")
     assert df.shape == (32639, 2)
-    os.remove(temp_test_file)
 
-    # human TRA
-    src.preprocessing.preprocess_vdjdb.filter_vdjdb(
-        input=Path("data/raw/vdjdb/vdjdb-all-species-tra-trb-non-paired.tsv"),
-        output=Path(temp_test_file),
+    # human TRB without spurious - vdjdb
+    df = src.preprocessing.preprocess_vdjdb.filter_vdjdb(
+        input=Path("data/raw/vdjdb/vdjdb-2019-08-08/vdjdb.txt"),
+        tcr_chain="TRB",
+        species="human",
+        mhc="all",
+        hla="all",
+        drop_spurious=False,
+    )
+    assert df.shape == (33028, 2)
+
+    # human TRB without spurious without 10x - vdjdb
+    df = src.preprocessing.preprocess_vdjdb.filter_vdjdb(
+        input=Path("data/raw/vdjdb/vdjdb-2019-08-08/vdjdb.txt"),
+        tcr_chain="TRB",
+        species="human",
+        mhc="all",
+        hla="all",
+        specific_removal=["KLGGALQAK", "10xgenomics"],
+    )
+    assert df.shape == (20024, 2)
+
+    # human TRA - vdjdb
+    df = src.preprocessing.preprocess_vdjdb.filter_vdjdb(
+        input=Path("data/raw/vdjdb/vdjdb-2019-08-08/vdjdb.txt"),
         tcr_chain="TRA",
         species="human",
         mhc="all",
         hla="all",
+        drop_spurious=False,
     )
-    df = pd.read_csv(temp_test_file, sep=";")
-    assert df.shape == (22248, 2)
-    os.remove(temp_test_file)
+    assert df.shape == (23564, 2)
 
-    # all species all chains
-    src.preprocessing.preprocess_vdjdb.filter_vdjdb(
-        input=Path("data/raw/vdjdb/vdjdb-all-species-tra-trb-non-paired.tsv"),
-        output=Path(temp_test_file),
+    # all species all chains with spurious - vdjdb
+    df = src.preprocessing.preprocess_vdjdb.filter_vdjdb(
+        input=Path("data/raw/vdjdb/vdjdb-2019-08-08/vdjdb.txt"),
+        tcr_chain="all",
+        species="all",
+        mhc="all",
+        hla="all",
+        drop_spurious=False,
+    )
+    assert df.shape == (61047, 2)
+
+    # all species all chains - vdjdb
+    df = src.preprocessing.preprocess_vdjdb.filter_vdjdb(
+        input=Path("data/raw/vdjdb/vdjdb-2019-08-08/vdjdb.txt"),
         tcr_chain="all",
         species="all",
         mhc="all",
         hla="all",
     )
-    df = pd.read_csv(temp_test_file, sep=";")
     assert df.shape == (59072, 2)
-    os.remove(temp_test_file)
