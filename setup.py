f826a70ef1ce0f18636d1d10b6219c24de9724e3
Pieter <13552343+pmoris@users.noreply.github.com>
2019-11-27 08:39:46 +0100

    Update setup.py
    Adds url and email info.

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='TCR-epitope recognition prediction using CNN approach',
+    url='https://github.com/pmoris/deepTCR',
     author='Pieter Moris',
+    author_email='pieter.moris@uantwerpen.be',
     license='MIT',
 )
