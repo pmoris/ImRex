9fd1eda011ce0c4462c7028d8afb52d7b64e59b3
Pieter <13552343+pmoris@users.noreply.github.com>
2020-03-11 16:32:32 +0100

    Make length restriction optional

--- src/processing/padded_batch_generator.py
+++ src/processing/padded_batch_generator.py
@@ -32,19 +32,24 @@ def padded_batch_generator(
     """ Standard PaddedBatchGenerator. """
     logger = logging.getLogger(__name__)
 
-    width = cdr3_range[1]
-    height = epitope_range[1]
-
     # filter on sequences on size
     if cdr3_range and epitope_range:
-        size_filter = SizeFilter(data_stream, cdr3_range, epitope_range)
+        width = cdr3_range[1]
+        height = epitope_range[1]
+        data_stream = SizeFilter(data_stream, cdr3_range, epitope_range)
         logger.info(f"Filtered CDR3 sequences to length: {cdr3_range}")
         logger.info(f"Filtered epitope sequences to length: {epitope_range}")
     else:
-        logger.info("No length filtering performed on sequences.")
+        sequence_pair, class_label = zip(*data_stream.data)
+        sequence_1, sequence_2 = zip(*sequence_pair)
+        width = len(max(list(sequence_1), key=len))
+        height = len(max(list(sequence_2), key=len))
+        logger.info(
+            f"No length filtering performed on sequences. Padding sequences up to {width} for cdr3 and {height} for epitope."
+        )
 
     # split into 3 streams: positive, negative and positive filter
-    input1, input2, input3 = tee(size_filter, amount=3)
+    input1, input2, input3 = tee(data_stream, amount=3)
 
     # create positive stream
     if not cache_images:
