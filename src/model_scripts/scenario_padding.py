354edd4241842fc4b9de98a76f7d776967724ac3
Pieter <13552343+pmoris@users.noreply.github.com>
2020-04-22 13:07:58 +0200

    Attempt to fix gpu oom error

--- src/model_scripts/scenario_padding.py
+++ src/model_scripts/scenario_padding.py
@@ -453,3 +453,7 @@ if __name__ == "__main__":
         val_data = val_data.batch(args.batch_size)
 
         trainer.train(model, train_data, val_data, iteration=iteration)
+
+        import tensorflow as tf
+
+        tf.keras.backend.clear_session()
